3768fe932474d1b5a2efa782f94702745f6ef050
sacul155 <cr1sram1rez107@gmail.com>
2026-03-10 00:27:36 -0400

    Solution added

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    newString = '';
+    for (let i = 0; i < num; i++) {
+        newString += str;
+    }
+    return newString;
 };
 
+console.log(repeatString('ball', 5));
 // Do not edit below this line
 module.exports = repeatString;
