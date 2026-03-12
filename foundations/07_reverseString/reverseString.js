cbba28b5a2ab6946666b6783d7ba45debb5b04b1
sacul155 <cr1sram1rez107@gmail.com>
2026-03-12 15:53:58 -0400

    (empty)

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString = str.at(i);
+        result = str.concat(newString);
+    }
+    return result;
 };
+
+console.log(reverseString('cat'));
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
